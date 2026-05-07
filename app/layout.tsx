2b9ece4e7e3cb63af7b30e92ae32cc192ac917e4
AdamCarter27 <adamcarter27@icloud.com>
2026-05-07 02:00:14 -0700

    finished demo

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,10 @@ export default function RootLayout({
         <Navbar />
         {children}
     </body>
+    <head>
+      <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+      <script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    </head>
 
     </html>
   );
